2496b7bbc947a54cddf447b70f3cd0de7eb35652
Maciej Rzadzinski <maciej.rzd@gmail.com>
2022-07-11 17:01:27 +0200

    Create files, structure with flexbox

---
+++ js.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+// Github icon animation
+const git = document.querySelector('#github');
+git.addEventListener('mouseover', (e) => git.classList.add('git'));
+git.addEventListener('mouseleave', (e) => git.classList.remove('git'));
