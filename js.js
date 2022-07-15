5d11cda310b4358a6518857704495aa7d36c98d1
Maciej Rzadzinski <maciej.rzd@gmail.com>
2022-07-15 18:38:47 +0200

    Add functionality to toggle switch

--- js.js
+++ js.js
@@ -37,11 +37,42 @@ slider.onchange = function() {
     createGrid(sliderValue);
 }
 
-// Update color picker color
+// Update color picker background color
 const colorPickerWrapper = document.querySelector('.color-picker-wrapper');
 const colorPicker = document.querySelector('.color-picker');
 
 colorPickerWrapper.style.backgroundColor = colorPicker.value;
 colorPicker.onchange = function() {
     colorPickerWrapper.style.backgroundColor = colorPicker.value;
+}
+
+// Toggle modes section
+const left = document.querySelector('.left');
+const normalModeButtons = document.querySelectorAll('.normal-mode');
+const toggle = document.querySelector('#checkbox');
+// Create monet-mode buttons
+const monetModeButton =  document.createElement('div');
+monetModeButton.classList.add('buttons', 'monet-mode');
+// To append new buttons create array -> append elements to DocumentFragment -> append to DOM button section -> select
+let monetModeButtonsFragment = document.createDocumentFragment();
+let monetModeButtonsArray = [];
+
+for (let i = 0; i < 8; i++) {
+    monetModeButtonsArray[i] = monetModeButton.cloneNode();
+}
+monetModeButtonsArray.forEach(button => monetModeButtonsFragment.appendChild(button));
+left.appendChild(monetModeButtonsFragment);
+let monetModeButtons = document.querySelectorAll('.monet-mode');
+
+toggle.addEventListener('change', ((e) => setTimeout(toggleModes, 100)));
+
+function toggleModes() {
+    if (toggle.checked) {
+        normalModeButtons.forEach(button => button.style.display = 'none');
+        monetModeButtons.forEach(button => button.style.display = 'block');
+
+    } else {
+        normalModeButtons.forEach(button => button.style.display = 'block');
+        monetModeButtons.forEach(button => button.style.display = 'none');
+    }
 }
